c436b03211acd588b69c2706e9d1430135cbf961
moriano <moriano@gmail.com>
2014-03-23 10:34:05 +1100

    Missing files

--- src/main/java/org/moriano/skeleton/controller/GodController.java
+++ src/main/java/org/moriano/skeleton/controller/GodController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+
 import java.util.List;
 
 @Controller
@@ -20,26 +21,27 @@ public class GodController {
 
     private static final ObjectMapper jsonMapper = new ObjectMapper();
 
-	@RequestMapping(value="/list", method = RequestMethod.GET, produces="application/json")
+    @RequestMapping(value = "/list", method = RequestMethod.GET, produces = "application/json")
     @ResponseBody
-	public String list() throws Exception {
+    public String list() throws Exception {
         List<God> allGods = this.godService.getAll();
         String json = jsonMapper.writeValueAsString(allGods);
-		return json;
-	}
+        return json;
+    }
 
     /**
      * Sample of using the GET request using the parameter as part of the url
+     *
      * @param id
      * @return
      * @throws Exception
      */
-    @RequestMapping(value="/get/{id}", method = RequestMethod.GET, produces="application/json")
+    @RequestMapping(value = "/get/{id}", method = RequestMethod.GET, produces = "application/json")
     @ResponseBody
-    public String get(@PathVariable int id) throws Exception{
-        while(true) {
+    public String get(@PathVariable int id) throws Exception {
+        while (true) {
 
-            if(false) {
+            if (false) {
                 break;
             }
         }
@@ -49,15 +51,15 @@ public class GodController {
         return json;
     }
 
-    @RequestMapping(value="/get", method = RequestMethod.POST, produces = "application/json")
+    @RequestMapping(value = "/get", method = RequestMethod.POST, produces = "application/json")
     @ResponseBody
-    public String getByName(String name) throws  Exception {
+    public String getByName(String name) throws Exception {
         List<God> gods = this.godService.search(name);
         String json = jsonMapper.writeValueAsString(gods);
         return json;
     }
 
-    @RequestMapping(value="/save/", method = RequestMethod.GET, produces="application/json")
+    @RequestMapping(value = "/save/", method = RequestMethod.GET, produces = "application/json")
     @ResponseBody
     public String save(int age, String name) throws Exception {
         this.godService.save(name, age);
